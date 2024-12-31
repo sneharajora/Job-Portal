dd047b3efb3eae310814d61bd496c7d6ed659fb6
Sneha <sneharajora0110@gmail.com>
2025-01-01 00:25:34 +0530

    add

---
+++ src/components/JobCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JobCard = ({job}) => {
+  return (
+    <div>
+        <div>
+            <img src="" alt=""></img>
+        </div>
+    </div>
+  )
+}
+
+export default JobCard

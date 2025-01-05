21c8344332013c22decb247a9e8cef54b5c8c70c
Sneha <sneharajora0110@gmail.com>
2025-01-06 01:26:43 +0530

    updated jobcard

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-
+import {useNavigate, useNavigation} from 'react-router-dom'
 const JobCard = ({job}) => {
+
+  const navigate = useNavigate()
   return (
     <div className='border p-6 shadow rounded'>
         <div className='flex justify-between items-center'>
@@ -13,8 +15,8 @@ const JobCard = ({job}) => {
            <span className='bg-red-50 border border-red-200 px-4 py-1.5 rounded'>{job.level}</span>
            <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
            <div className='mt-4 flex gap-4 text-sm'>
-             <button  className='bg-blue-600 text-white px-4 py-2 rounded'>Apply Now</button>
-             <button className='text-gray-500 border border-gray-500 px-4 py-2 rounded'>Learn more</button>
+             <button onClick={()=>{navigate(`/apply-jobs/${job._id}`);scrollTo(0,0)}} className='bg-blue-600 text-white px-4 py-2 rounded'>Apply Now</button>
+             <button onClick={()=>{navigate(`/apply-jobs/${job._id}`);scrollTo(0,0)}} className='text-gray-500 border border-gray-500 px-4 py-2 rounded'>Learn more</button>
            </div>
         </div>
     </div>

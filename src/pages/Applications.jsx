63b34337827aa4d683ffb832e8b6639a1ff2c1e1
Sneha <sneharajora0110@gmail.com>
2025-01-17 23:05:57 +0530

    added morejobs

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Navbar from '../components/Navbar'
 
 const Applications = () => {
+
+  const[isEdit ,setEdit]= useState(false)
   return (
-    <div>Applications</div>
+   <>
+     <Navbar/>
+     <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+      <h2 className='text-xl font-semibold'>Your Resume</h2>
+      <div className='flex gap-2 mb-6 mt-3'>
+       {
+        isEdit?
+        <>
+
+        </>: 
+         <div>
+           <a href="">
+            Resume
+           </a>
+           <button>
+            Edit
+           </button>
+         </div>
+       }
+      </div>
+     </div>
+   </>
   )
 }
 

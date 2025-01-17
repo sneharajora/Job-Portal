217042e695f53bf7f42ae4742adc4a6fe02d19ad
Sneha <sneharajora0110@gmail.com>
2025-01-17 23:04:36 +0530

    newnav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
+  const navigate = useNavigate()
+
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between item-center">
-        <img src={assets.logo} alt="" />
+        <img onClick={()=>navigate('/')} className="cursor-pointer"  src={assets.logo} alt="" />
         {user ? (
           <div className="flex item-center gap-3">
             <Link to={'/applications'}>Applied Jobs</Link>

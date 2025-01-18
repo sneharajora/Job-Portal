f061ca0ce754b4c8339513af23de5c341fe3b496
Sneha <sneharajora0110@gmail.com>
2025-01-18 23:44:51 +0530

    added new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { AppContext } from './context/AppContext'
 import {Route,Routes} from 'react-router-dom'
 import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import Home from './pages/Home'
-
+import RecruiterLogin from './components/RecruiterLogin'
 const App = () => {
-  return (
+  const {showRecruiterLogin} = useContext(AppContext)
+   return (
     <div>
+       {showRecruiterLogin  && <RecruiterLogin/>}
       <Routes>
         <Route path='/' element={<Home/>}>Home</Route>
         <Route path='/apply-jobs/:id' element={<ApplyJob/>}>Apply</Route>
